c3606db1872ba3907ac2810b04930fbdad77eab7
Daniel Lucht <daniel.lucht@uni-bielefeld.de>
2024-09-24 16:56:48 +0200

    Refactored preprocessing script. E.g. used fluidsynth instead of pm.synthesize()

--- src/extract_predominant_melody.py
+++ src/extract_predominant_melody.py
@@ -1,52 +1,72 @@
 import argparse
 import glob
 import os
-from pathlib import Path
+import pickle
+import time
 import warnings
+from copy import deepcopy
+from pathlib import Path
 
 import essentia.standard as es
+import mido
 import numpy as np
 import pretty_midi
 
-import mido
-
-import time
-
-from copy import deepcopy
-
 SAMPLE_RATE = 44100.0
 
-def test_synthesis(file_path):
+
+# Global for now
+eqloud = es.EqualLoudness()  # recommended as preprocessing for melody extraction
+melody_extractor = es.PredominantPitchMelodia(guessUnvoiced=True)
+pcs = es.PitchContourSegmentation(hopSize=128)
+
+
+def test_synthesis(file_path, out_path):
   pm = pretty_midi.PrettyMIDI(file_path)
 
   # Loudness equalization
-  eqloud = es.EqualLoudness()
+  # eqloud = es.EqualLoudness()
 
   synthed_synthesize = pm.synthesize().astype(np.float32)
-  writer = es.MonoWriter(filename='temp/synthtest/eq_synthesize.wav', sampleRate=SAMPLE_RATE, format='wav')
+  writer = es.MonoWriter(filename=f'{out_path}/eq_synthesize.wav', sampleRate=SAMPLE_RATE, format='wav')
   writer(eqloud(synthed_synthesize))
 
   synthed_fluidsynth = pm.fluidsynth().astype(np.float32)
-  writer = es.MonoWriter(filename='temp/synthtest/eq_fluidsynth.wav', sampleRate=SAMPLE_RATE, format='wav')
+  writer = es.MonoWriter(filename=f'{out_path}/eq_fluidsynth.wav', sampleRate=SAMPLE_RATE, format='wav')
   writer(eqloud(synthed_fluidsynth))
 
-def extract_from_midi_to_midi(file_path, output):
+def extract_from_midi_to_midi(file_path, output, use_cache=True):
   """
   Extracts the melody of a midi file. Stores the melody and accompaniment as separate files
   :param file_path: Input midi file
   :param output: The base path to store the files in
+  :param use_cache: Whether to use cached melody notes
   :return: None
   """
-
   try:
     pm_original = pretty_midi.PrettyMIDI(midi_file=file_path)
-    pm_original.remove_invalid_notes()
+    # pm_original.remove_invalid_notes()  # optional
   except (OSError, EOFError, ValueError, KeyError, mido.midifiles.meta.KeySignatureError) as err:
     print(f'ERROR Corrupt midi file {file_path}. Skipping it.')
     print('Cause of corruption:', str(err))
   else:
-    # Productive
-    melody_notes = compute_melody_notes(pm_original)
+    # With clean_midi (lmd) as input:
+    p = Path(file_path)  # e. g. '../clean_midi/Electric Light Orchestra/Mr Blue Sky.mid'
+    nfile_path = f'{p.parts[-2]} - {p.parts[-1]}'  # e. g. 'Electric Light Orchestra - Mr Blue Sky.mid'
+
+    if use_cache:
+      # (Hardcoded for now)
+      cache_file_path = os.path.join('note_cache', nfile_path)
+      try:
+        note_cache = pickle.load(open(cache_file_path, 'rb'))
+        # cache_file_name
+        melody_notes = note_cache['melody_notes']
+      except Exception:  # as ex:
+        print('Not cached. Computing notes')
+        melody_notes = precompute_synthesized_notes(file_path, cache_file_path, loaded_pm=pm_original)
+    else:
+      melody_notes = compute_melody_notes(pm_original)
+
     # Copies all time_signature changes, etc. from the original midi file
     pm_melody = deepcopy(pm_original)
 
@@ -61,7 +81,7 @@ def extract_from_midi_to_midi(file_path, output):
 
     time_thresh_start = 2
     time_thresh_end = 2
-    pitch_thresh = 3  # can (and should) stay small because values are quantized!
+    pitch_thresh = 3  # can (and should) stay small because values are quantized already!
 
     processed_orig_notes = 0
     removed_notes = 0
@@ -81,7 +101,7 @@ def extract_from_midi_to_midi(file_path, output):
     # Debugging:
     # Tracks how many melody notes have been assigned to an original note with bools for each idx
     m_n_assigned = np.zeros(len(melody_notes), dtype=bool)
-    #------------------------------------------------------
+    # ------------------------------------------------------
     for instr in pm_original.instruments:
       if not instr.is_drum:
         # Debugging
@@ -101,8 +121,7 @@ def extract_from_midi_to_midi(file_path, output):
           mel_instrument_set = False
           mel_note_block_found = False
           # o_note is the original note of the instrument
-          for o_note_idx, o_note in enumerate(instr.notes[:]):
-          # for o_note_idx, o_note in enumerate(instr.notes[found_mel_note_idx:], start=found_mel_note_idx):
+          for o_note_idx, o_note in enumerate(instr.notes[found_mel_note_idx:], start=found_mel_note_idx):
             # m_note and o_note are close
             if _filter_condition(melody_note=m_note, orig_note=o_note):
               # Collect indices of o_notes that are found in the melody notes
@@ -121,14 +140,13 @@ def extract_from_midi_to_midi(file_path, output):
                 found_mel_note_idx = o_note_idx
                 # Remember whether a note already matched for processing the following o_notes
                 mel_note_block_found = True
-                # Set velocity of melody note according to the matching non-melody note
-                m_note.velocity = o_note.velocity
 
-                m_note.start, m_note.end = o_note.start, o_note.end
-                m_note.pitch = o_note.pitch
-
+                # Fit the melody note's values to the matching non-melody note
+                fitted_m_note = pretty_midi.Note(velocity=o_note.velocity, pitch=o_note.pitch, start=o_note.start, end=o_note.end)
                 # Add melody note to fitting instrument
-                new_mel_instrument.notes.append(m_note)
+                # Workaround to make sure no melody notes are duplicated
+                if len(new_mel_instrument.notes) == 0 or repr(fitted_m_note) != repr(new_mel_instrument.notes[-1]):
+                  new_mel_instrument.notes.append(fitted_m_note)
                 mel_instrument_set = True
             # m_note is parted from o_note according to the thresholds
             else:
@@ -139,15 +157,14 @@ def extract_from_midi_to_midi(file_path, output):
                 break
 
           # If velocity is still at the default value, take the value of the previous m_note
-          if m_note.velocity == -1:
-            if m_note_idx == 0:  # If the current melody note is the first and did not match any o_note:
-              m_note.velocity = instr.notes[0].velocity  # take the first o_note's velocity
-            else:
-              m_note.velocity = melody_notes[m_note_idx - 1].velocity
+          # if m_note.velocity == -1:
+          #   if m_note_idx == 0:  # If the current melody note is the first and did not match any o_note:
+          #     m_note.velocity = instr.notes[0].velocity  # take the first o_note's velocity
+          #   else:
+          #     m_note.velocity = melody_notes[m_note_idx - 1].velocity
 
         # Add the melody notes with the fitting instrument again
         if len(new_mel_instrument.notes) > 0:
-          pm_melody.remove_invalid_notes()  # optional
           pm_melody.instruments.append(new_mel_instrument)
 
         instr.notes = np.delete(np.array(instr.notes), list(o_notes_to_delete))
@@ -161,10 +178,6 @@ def extract_from_midi_to_midi(file_path, output):
 
     print('Unassigned m_notes:', len(np.argwhere(m_n_assigned == False)))
 
-    # With clean_midi (lmd) as input:
-    p = Path(file_path)
-    nfile_path = f'{p.parts[-2]} - {p.parts[-1]}'  # with clean_midi (lmd) as input
-
     # Save to midi file
     out_path_mel = os.path.join(output, f'{Path(nfile_path).stem}_melody.mid')
     pm_melody.write(out_path_mel)
@@ -177,25 +190,21 @@ def extract_from_midi_to_midi(file_path, output):
     print('Accompaniment result written to', out_path_accomp)
 
 
-def compute_melody_notes(pm_original):
+def compute_melody_notes(pm_original, use_fluidsynth=True):
   """
   Uses MELODIA and pitch contour segmentation to compute the midi notes from pretty_midi object
   :param pm_original: The midi object
+  :param use_fluidsynth: Whether to use fluidsynth. Is slower but more realistic.
   :return: midi melody notes
   :rtype: list[pretty_midi.Note]
   """
-
-  # synth = pm_original.fluidsynth().astype(np.float32)  # float32 is essentia's internal datatype; fluidsynth would output float64
-  synth = pm_original.synthesize().astype(np.float32)
-  eqloud = es.EqualLoudness()  # recommended as preprocessing for melody extraction
+  if use_fluidsynth:
+    synth = pm_original.fluidsynth().astype(np.float32)  # float32 is essentia's internal datatype; fluidsynth would output float64
+  else:
+    synth = pm_original.synthesize().astype(np.float32)
   audio_data = eqloud(synth)
-  melody_extractor = es.PredominantPitchMelodia(guessUnvoiced=True)
   pitch_values, pitch_confidence = melody_extractor(audio_data)  # pitch values from unvoiced segments are negative
-
-  # Note that PitchContourSegmentation already returns the quantized midi note numbers, not the physical pitches
-  # returns onset times in seconds, already quantized to the equal tempered scale
-  # Pitch values to pm
-  onsets, durations, notes = es.PitchContourSegmentation(hopSize=128)(list(np.absolute(pitch_values)), audio_data)
+  onsets, durations, notes = pcs(pitch_values, audio_data)
   melody_notes = []
   for pitch, onset, duration in zip(notes, onsets, durations):
     # NOTE velocity = -1 is a default value in this context and is changed later. Otherwise -1 is an invalid velocity value
@@ -204,18 +213,24 @@ def compute_melody_notes(pm_original):
   return melody_notes
 
 
-def process_folder(folder, output):
-  # input_files = glob.glob(os.path.join(folder, '**/*.mp3'), recursive=True)
-  input_files = glob.glob(os.path.join(folder, '**/*.mid'), recursive=True)
-  os.makedirs(output, exist_ok=True)
+def process_folder(folder, output_folder, recursive=True):
+  """
+  Batch process a folder of midi tracks.
+  :param folder: Input midi folder
+  :param output_folder: Folder to write output to
+  :param recursive: If true process all midi tracks recursively
+  :return: None
+  """
+  input_files = glob.glob(os.path.join(folder, '**/*.mid'), recursive=recursive)  # ~ 19,4k in total in clean midi set
+  print(f'Processing {len(input_files)} midi files')
+  os.makedirs(output_folder, exist_ok=True)
   for file_path in input_files:
-    # extract_from_mp3_to_midi(file_path)
     # Assuming that _melody.mid is also present when _accompaniment.mid is
     
     # Converts folder structures like 'artist/song.mid' to 'artist - song.mid'
     p = Path(file_path)
     planned_outpath = f'{p.parts[-2]} - {p.parts[-1]}'  # with clean_midi (lmd) as input
-    possibly_existing_outfile_path = os.path.join(output, f'{Path(planned_outpath).stem}_accompaniment.mid')
+    possibly_existing_outfile_path = os.path.join(output_folder, f'{Path(planned_outpath).stem}_accompaniment.mid')
     # breakpoint()
     if not os.path.exists(possibly_existing_outfile_path):
       # if not file_path in ('../lmd_full/e/e3de19fed976d515437604f7ac0d34a4.mid', '../lmd_full/f/fd93b44a6e334ec2d9ead40a92dcca52.mid', '../lmd_full/e/e363d6d94eb8ad76fa6694f31ec6c79d.mid', '../lmd_full/0/069a3ce3c45b7e15f46138c5e5a67469.mid', '../lmd_full/f/f94b0d40a3388bf932504d1906bec35e.mid', '../lmd_full/e/e7999e9c714f39adfbd9775794daa46b.mid', '../lmd_full/0/0531e73378d4fc4f86ffad4bce283f5a.mid', '../lmd_full/e/e2c42da8bccf9c8067d6bc3af9b957e6.mid', '../lmd_full/0/00a6fcf5afc65dee2a833291b4c11b0c.mid'):
@@ -223,14 +238,74 @@ def process_folder(folder, output):
       print('Processing', file_path)
       if len(planned_outpath) < 210:  # Prevent error for output filename being too long upon saving the result
         try:
-          extract_from_midi_to_midi(file_path, output)
+          extract_from_midi_to_midi(file_path, output_folder)
         except RuntimeWarning as w:
           print(f'Skipping {file_path}. RuntimeWarning encountered:', w)
       else:
         print(f'Skipping {file_path}. Output name presumably too long. Len:', len(planned_outpath))
     else:
-      print(f'Skipping {file_path}. Already in {output}')
+      print(f'Skipping {file_path}. Already in {output_folder}')
 
+
+def precompute_synthesized_notes(file_path, cache_file_name, loaded_pm=None):
+  """
+  Computes Midi notes from input file and caches it.
+  Note that these notes have a velocity of -1. It is expected to be recomputed later on!
+  Cache file has the structure {'filename': <file_path>, 'melody_notes': <computed pretty_midi.Notes>}
+  :param file_path: Input midi file path
+  :param cache_file_name: Cache file path
+  :param loaded_pm: Optionally loaded PrettyMIDI object. Is loaded here if None.
+  :return: None
+  """
+  try:
+    if loaded_pm is None:
+      pm = pretty_midi.PrettyMIDI(midi_file=file_path)
+      pm.remove_invalid_notes()
+    else:
+      pm = loaded_pm
+  except (OSError, EOFError, ValueError, KeyError, mido.midifiles.meta.KeySignatureError) as err:
+    print(f'ERROR Corrupt midi file {file_path}. Skipping it.')
+    print('Cause of corruption:', str(err))
+  else:
+    storage = {
+      'filename': file_path,
+      'melody_notes': compute_melody_notes(pm, use_fluidsynth=True),
+    }
+    pickle.dump(storage, open(cache_file_name, 'wb'))
+    return storage['melody_notes']
+
+
+def precompute_batch(folder, output_path, recursive=True):
+  input_files = glob.glob(os.path.join(folder, '**/*.mid'), recursive=recursive)  # [:10000]
+  num_files = len(input_files)
+  print(f'Processing {num_files} midi files')
+  os.makedirs(output_path, exist_ok=True)
+  num_skipped_files = 0
+
+  for idx, file_path in enumerate(input_files):
+    print(f'[{idx}/{num_files}]\t{file_path}', end='\t')
+    p = Path(file_path)
+    planned_outpath = f'{p.parts[-2]} - {p.parts[-1]}'  # with clean_midi (lmd) as input
+    possibly_existing_outfile_path = os.path.join(output_path, f'{Path(planned_outpath).stem}.mid')
+    if not os.path.exists(possibly_existing_outfile_path):
+      if len(planned_outpath) < 210:  # Prevent error for output filename being too long upon saving the result
+        try:
+          precompute_synthesized_notes(file_path, possibly_existing_outfile_path)
+        except RuntimeWarning as w:
+          print(f'Skipping {file_path}. RuntimeWarning encountered:', w)
+          num_skipped_files += 1
+          continue
+      else:
+        print(f'Skipping {file_path}. Output name presumably too long. Len:', len(planned_outpath))
+        num_skipped_files += 1
+        continue
+    else:
+      print(f'Skipping {file_path}. Already in {output_path}')
+      num_skipped_files += 1
+      continue
+
+    print('Done')
+  print('Skipped files:', num_skipped_files)
 
 def main():
   # Convert RuntimeWarnings to errors so the can be caught in process_folder
@@ -242,7 +317,14 @@ def main():
 
   start_time = time.time()
 
-  process_folder(folder=args.input, output=args.output)
+  # test_synthesis(args.input, args.output)
+  # precompute_batch(args.input, args.output)
+  # extract_from_midi_to_midi(args.input, args.output)
+
+  # Prod
+  process_folder(folder=args.input, output_folder=args.output)
+
+  # extract_from_midi_to_midi(args.input, args.output)
 
   end_time = time.time()
 
